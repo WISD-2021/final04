a1179425cc3f49f3af679a304a2de973d6fc258b
Xin-Ting Ke <greenpepper0606@gmail.com>
2022-01-05 11:12:50 +0800

    建立reserves外來鍵

---
+++ database/migrations/2022_01_05_031011_add_foreign_member_id_to_reserves.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignMemberIdToReserves extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reserves', function (Blueprint $table) {
+            $table->foreign('member_id')->references('id')->on('members');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reserves', function (Blueprint $table) {
+            //
+        });
+    }
+}
