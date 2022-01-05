0882b874e828b6990c793505defb002735b036ee
Xin-Ting Ke <greenpepper0606@gmail.com>
2022-01-05 10:28:35 +0800

    修改members欄位

--- database/migrations/2022_01_05_022341_create_members_table.php
+++ database/migrations/2022_01_05_022341_create_members_table.php
@@ -15,6 +15,8 @@ class CreateMembersTable extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->id('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
