f3239a35fd7c3e5ae7b14b45911daac92d9c466b
Xin-Ting Ke <greenpepper0606@gmail.com>
2022-01-05 10:34:04 +0800

    修改orders欄位

--- database/migrations/2022_01_05_022505_create_orders_table.php
+++ database/migrations/2022_01_05_022505_create_orders_table.php
@@ -15,6 +15,11 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->Integer('member_id');
+            $table->foreign('member_id')->references('id')->on('members');
+            $table->string('method');
+            $table->Integer('total');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
