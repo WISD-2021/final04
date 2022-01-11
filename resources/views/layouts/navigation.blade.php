613e6e61c796c5773b29e15dc47616a1cb9c2445
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2022-01-11 23:58:45 +0800

    判斷使用者

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,23 @@
                 <li class="nav-item"><a class="nav-link" style="color:black" href="{{ route('item') }}">菜單</a></li>
                 <li class="nav-item"><a class="nav-link" style="color:black" href="{{ route('reserve') }}">線上預約</a></li>
                 <li class="nav-item"><a class="nav-link" style="color:black" href="{{ route('order') }}">訂單</a></li>
-                <li class="nav-item"><a class="nav-link" style="color:black" href="{{ route('user.logout') }}">登出</a></li>
-                <li class="nav-item"><a class="nav-link" style="color:black" href="{{ route('register') }}">註冊</a></li>
+                @if(\Illuminate\Support\Facades\Auth::check())
+                    @if(Auth::user()->type == '0')
+                        <script>alert('管理者登入成功');window.location.href='{{ route('admin.dashboard.index') }}'</script>
+                    @else
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="{{ route('user.logout') }}">登出</a></li>
+                        </ul>
+                        </li>
+                    @endif
+                @else
+                        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登入</a></li>
+                        @if (Route::has('register'))
+                        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">註冊</a></li>
+                        @endif
+                @endif
                 </ul>
             </div>
         </div>
